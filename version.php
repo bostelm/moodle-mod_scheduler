aa36fa844d13cfe1c5051fd7ea29ea951b3c379b
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2013-09-27 19:05:55 +0100

    bumping version number for 2.5 release

--- version.php
+++ version.php
@@ -5,20 +5,20 @@
  *
  * @package    mod
  * @subpackage scheduler
- * @copyright  2011 Henning Bostelmann and others (see README.txt)
+ * @copyright  2013 Henning Bostelmann and others (see README.txt)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * This is the development branch (master) of the scheduler module.
+ * This is the Moodle 2.5 branch of the scheduler module.
  */
 
-$module->version  = 2012102903;       // The current module version (Date: YYYYMMDDXX)
-$module->release  = '2.x dev';        // Human-friendly version name
+$module->version  = 2013092700;       // The current module version (Date: YYYYMMDDXX)
+$module->release  = '2.5.0';          // Human-friendly version name
 $module->requires = 2013051400;       // Requires Moodle 2.5
-$module->maturity = MATURITY_ALPHA;   // Alpha development code - not for production sites
+$module->maturity = MATURITY_STABLE;  // Stable branch
 
 $module->cron     = 60;               // Period for cron to check this module (secs)
 
