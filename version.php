cd69bad7fbbfce8f8206c433c551ac35377d87f0
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2014-09-05 18:28:01 +0100

    Bumping version number for 2.3.2 release

--- version.php
+++ version.php
@@ -13,11 +13,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /**
  * This is the 2.3 branch (MOODLE_23_STABLE) of the scheduler module.
- * It is intended for use with Moodle 2.3 and above.
+ * It is intended for use with Moodle 2.3 and 2.4.
  */
 
-$module->version  = 2012102803;       // The current module version (Date: YYYYMMDDXX)
-$module->release  = '2.3.1+';         // Human-friendly version name
+$module->version  = 2012102804;       // The current module version (Date: YYYYMMDDXX)
+$module->release  = '2.3.2';         // Human-friendly version name
 $module->requires = 2012062500;       // Requires Moodle 2.3
 $module->maturity = MATURITY_STABLE;  // Stable release
 
