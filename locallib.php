6e6ed457e07868344045d276f87ce13efb645839
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2014-09-14 15:14:27 +0100

    CONTRIB-5300 Notifications: use time zone of the recipient

--- locallib.php
+++ locallib.php
@@ -161,6 +161,41 @@ function scheduler_delete_calendar_events($slot) {
     //this return may not be meaningful if the delete records functions do not return anything meaningful.
 }
 
+/**
+ * Returns a float or a string which denotes a given user's timezone.
+ *
+ * A float value means that a simple offset from GMT is used, while a string (it will be the name of a timezone in the database)
+ * means that for this timezone there are also DST rules to be taken into account.
+ * Checks various settings and picks the most dominant of those which have a value.
+ *
+ * This is mainly a copy of lib/moodlelib.php, function get_user_timezone(),
+ * but with an additional parameter to specify a user (other than the current one)
+ * for whom the setting is computed.
+ *
+ * @param float|int|string $tz timezone to calculate GMT time offset before
+ *        calculating user timezone, 99 is default user timezone
+ *        {@link http://docs.moodle.org/dev/Time_API#Timezone}
+ * @return float|string
+ */
+function scheduler_get_user_timezone(stdClass $user, $tz = 99) {
+    global $CFG;
+
+    $timezones = array(
+        $tz,
+        isset($CFG->forcetimezone) ? $CFG->forcetimezone : 99,
+        isset($user->timezone) ? $user->timezone : 99,
+        isset($CFG->timezone) ? $CFG->timezone : 99,
+        );
+
+    $tz = 99;
+
+    // Loop while $tz is, empty but not zero, or 99, and there is another timezone is the array.
+    while (((empty($tz) && !is_numeric($tz)) || $tz == 99) && $next = each($timezones)) {
+        $tz = $next['value'];
+    }
+    return is_numeric($tz) ? (float) $tz : $tz;
+}
+
 
 /**
  * Construct an array with subtitution rules for mail templates, relating to
@@ -181,6 +216,8 @@ function scheduler_get_mail_variables (scheduler_instance $scheduler, scheduler_
     // Force any string formatting to happen in the target language.
     $oldlang = force_current_language($lang);
 
+    $tz = scheduler_get_user_timezone($recipient);
+
     $vars = array();
 
     if ($scheduler) {
@@ -188,9 +225,9 @@ function scheduler_get_mail_variables (scheduler_instance $scheduler, scheduler_
         $vars['STAFFROLE']  = $scheduler->get_teacher_name();
     }
     if ($slot) {
-        $vars ['DATE']     = userdate($slot->starttime, get_string('strftimedate'));
-        $vars ['TIME']     = userdate($slot->starttime, get_string('strftimetime'));
-        $vars ['ENDTIME']  = userdate($slot->endtime, get_string('strftimetime'));
+        $vars ['DATE']     = userdate($slot->starttime, get_string('strftimedate'), $tz);
+        $vars ['TIME']     = userdate($slot->starttime, get_string('strftimetime'), $tz);
+        $vars ['ENDTIME']  = userdate($slot->endtime, get_string('strftimetime'), $tz);
         $vars ['LOCATION'] = format_string($slot->appointmentlocation);
     }
     if ($attendant) {
