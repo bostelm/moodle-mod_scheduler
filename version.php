9eed465959960011c0bd607aa2100732f8806cc3
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2015-10-29 18:51:23 +0000

    Updating version number for 2.9 release

--- version.php
+++ version.php
@@ -16,9 +16,9 @@ defined('MOODLE_INTERNAL') || die();
  */
 
 $plugin->component = 'mod_scheduler'; // Full name of the plugin (used for diagnostics)
-$plugin->version   = 2015071500;      // The current module version (Date: YYYYMMDDXX)
-$plugin->release   = '2.x dev';       // Human-friendly version name
+$plugin->version   = 2015102900;      // The current module version (Date: YYYYMMDDXX)
+$plugin->release   = '2.9.0';       // Human-friendly version name
 $plugin->requires  = 2015042800;      // Requires Moodle 2.9
-$plugin->maturity  = MATURITY_ALPHA;  // Alpha development code - not for production sites
+$plugin->maturity  = MATURITY_STABLE;  // Alpha development code - not for production sites
 
 $plugin->cron     = 60;               // Period for cron to check this module (secs)
