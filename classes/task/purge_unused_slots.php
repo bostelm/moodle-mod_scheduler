c7035874c2df37d66bd481e3eb902dbc468955ac
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2016-05-17 11:54:58 +0100

    Adding new scheduled tasks

---
+++ classes/task/purge_unused_slots.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Scheduled background task for sending automated appointment reminders
+ *
+ * @package    mod_scheduler
+ * @copyright  2016 Henning Bostelmann and others (see README.txt)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_scheduler\task;
+
+require_once(dirname(__FILE__).'/../../model/scheduler_instance.php');
+
+class purge_unused_slots extends \core\task\scheduled_task {
+    public function get_name() {
+        return get_string('purgeunusedslots', 'mod_scheduler');
+    }
+
+    public function execute() {
+        \scheduler_instance::free_late_unused_slots();
+    }
+}
