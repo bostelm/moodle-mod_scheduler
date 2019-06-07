ca8a5a408d084929ff442b0513b0c8d5773040ac
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2019-06-07 20:01:25 +0100

    Bump version number for 3.5.1 release

--- version.php
+++ version.php
@@ -15,7 +15,7 @@ defined('MOODLE_INTERNAL') || die();
  */
 
 $plugin->component = 'mod_scheduler'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 2018112600;      // The current module version (Date: YYYYMMDDXX).
-$plugin->release   = '3.5.0';         // Human-friendly version name.
+$plugin->version   = 2018112602;      // The current module version (Date: YYYYMMDDXX).
+$plugin->release   = '3.5.1';         // Human-friendly version name.
 $plugin->requires  = 2018051700;      // Requires Moodle 3.5.
 $plugin->maturity  = MATURITY_STABLE; // Stable release
