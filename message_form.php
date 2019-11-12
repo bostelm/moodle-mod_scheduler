c71e45c685b0fcd491a56aa8c3cade28e3bf7d5c
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2019-11-12 10:54:20 +0000

    Messaging: Add missing "use"

--- message_form.php
+++ message_form.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use mod_scheduler\model\scheduler;
+
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->libdir.'/formslib.php');
