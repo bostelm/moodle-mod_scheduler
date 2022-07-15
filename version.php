d11543cbe473951146b1ec16ba00ded99f980f46
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2022-07-15 20:14:08 +0200

    Now developing for Moodle 4.x

--- version.php
+++ version.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
  */
 
 $plugin->component = 'mod_scheduler'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 2019120300;      // The current module version (Date: YYYYMMDDXX).
-$plugin->release   = '3.x dev';       // Human-friendly version name.
-$plugin->requires  = 2019052000;      // requires Moodle 3.7.
+$plugin->version   = 2022071500;      // The current module version (Date: YYYYMMDDXX).
+$plugin->release   = '4.x dev';       // Human-friendly version name.
+$plugin->requires  = 2022041900;      // requires Moodle 4.0.
 $plugin->maturity  = MATURITY_ALPHA;  // Development release - not for production use.
