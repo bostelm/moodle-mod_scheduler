62dc7b2639e6807be145e0acfb297420f5aeb973
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2023-08-25 12:11:55 +0100

    CONTRIB-9345 fix deprecated functions in cron

--- classes/task/send_reminders.php
+++ classes/task/send_reminders.php
@@ -74,12 +74,12 @@ class send_reminders extends \core\task\scheduled_task {
 
             // Send reminder to all students in the slot.
             foreach ($slotm->get_appointments() as $appointment) {
-                $student = $DB->get_record('user', array('id' => $appointment->studentid));
-                cron_setup_user($student, $course);
+                $student = \core_user::get_user($appointment->studentid);
+                \core\cron::setup_user($student, $course);
                 \scheduler_messenger::send_slot_notification($slotm,
                         'reminder', 'reminder', $teacher, $student, $teacher, $student, $course);
             }
         }
-        cron_setup_user();
+        \core\cron::setup_user();
     }
 }
