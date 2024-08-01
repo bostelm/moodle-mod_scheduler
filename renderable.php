e28c2fb806261cc677f83ecfee613e5dcc4ac564
henning.bostelmann <hb@bostelm.eu>
2024-08-01 13:19:03 +0200

    CONTRIB-9626 remove unused property from action_link

--- renderable.php
+++ renderable.php
@@ -348,7 +348,6 @@ class scheduler_command_bar implements renderable {
             $confirmaction = new confirm_action(get_string($confirmkey, 'scheduler'));
         }
         $act = new action_link($url, $title, $confirmaction, $attributes, $pix);
-        $act->primary = false;
         return $act;
     }
 
