b4d9c0af5c8b6bb6bf80fcd7de5544695604959b
lenaherf <lherfeld@uni-muenster.de>
2026-08-14 17:58:58 +0200

    Remove the limited width again, because it makes the tab placement jump around. Instead center only the contents a bit more.

--- export.php
+++ export.php
@@ -75,6 +75,9 @@ if (!$data || $preview) {
     $taburl = new moodle_url('/mod/scheduler/view.php', ['id' => $scheduler->cmid, 'what' => 'export']);
     echo $output->teacherview_tabs($scheduler, $permissions, $taburl, 'export');
 
+    // Only the contents below the navigation tabs is narrowed.
+    echo html_writer::start_div('scheduler-limited-content');
+
     if ($groupmode) {
         groups_print_activity_menu($scheduler->cm, $taburl);
     }
@@ -104,6 +107,9 @@ if (!$data || $preview) {
         echo html_writer::div(get_string('previewlimited', 'scheduler', $limit), 'previewlimited');
     }
 
+    // Close the limited width div.
+    echo html_writer::end_div();
+    // Finish the page.
     echo $output->footer();
     exit();
 }
