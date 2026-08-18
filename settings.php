034f3910c2658ef7263fba87cb468633b1510edd
lenaherf <lherfeld@uni-muenster.de>
2026-08-18 14:14:43 +0200

    codechecker

--- settings.php
+++ settings.php
@@ -72,9 +72,9 @@ if ($ADMIN->fulltree) {
     ));
 
     $settings->add(new admin_setting_configcheckbox(
-            'mod_scheduler/keepattendedincalendar',
-            get_string('keepattendedincalendar', 'scheduler'),
-            get_string('keepattendedincalendar_desc', 'scheduler'),
-            0
+        'mod_scheduler/keepattendedincalendar',
+        get_string('keepattendedincalendar', 'scheduler'),
+        get_string('keepattendedincalendar_desc', 'scheduler'),
+        0
     ));
 }
