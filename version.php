d39a5a6966e13a35a2dd02c5896709e8d0ca733d
Michael Kotlyar <michael.kotlyar@catalyst-eu.net>
2026-09-14 11:54:04 +0100

    Bug fixes and improvements

--- version.php
+++ version.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
  */
 
 $plugin->component = 'mod_scheduler'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 2026080400;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2026080401;      // The current module version (Date: YYYYMMDDXX).
 $plugin->release   = 'v5.2-r1';
 $plugin->requires  = 2024100712;      // Requires Moodle 4.5.
 $plugin->supported = [500, 502];
